320ebef6afa3cd52b2606353b7b4b24f04e87064
Cyprien DIOT <cyprien.diot@pmsipilot.com>
2015-08-12 17:05:41 +0200

    docker restructuration

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
 concat = require('gulp-concat'),
+concatutil = require('gulp-concat-util');
 output = './public',
 fs = require('fs'),
 del = require('del'),
+uglify = require('gulp-uglify')
 ngAnnotate = require('gulp-ng-annotate');
 
 var config = {
@@ -17,6 +19,8 @@ var config = {
 
     outputVendorsCssFile: 'vendor.css',
 
+    outputAppCssFile: 'app.css',
+
     outputFontsDir: output + '/fonts',
 
     appJsEntry: [
@@ -25,17 +29,22 @@ var config = {
         'js/controller/*.js'
     ],
 
+    appCssEntry: [
+	'css/*.css',
+    ],
+
+
     vendorsJsEntry: [
         'node_modules/jquery/dist/jquery.js',
         'node_modules/angular/angular.js',
         'node_modules/angular-route/angular-route.js',
         'node_modules/angular-cookies/angular-cookies.js',
-        'node_modules/angular-translate/angular-translate.js',
+        'node_modules/angular-translate/dist/angular-translate.js',
         'node_modules/favico.js/favico.js',
         'node_modules/bootstrap/dist/js/bootstrap.js',
         'node_modules/angular-slider/slider.js',
         'node_modules/ng-table/dist/ng-table.js',
-        'node_modules/angular-xeditable/dist/js/xeditable.js'
+        'node_modules/angular-xeditable/xeditable.min.js'
     ],
     vendorsCssEntry: [
 	'node_modules/bootstrap/dist/css/bootstrap.css',
@@ -47,16 +56,16 @@ var config = {
 
     fontsEntry: [
         'node_modules/bootstrap/fonts/*',
-        'node_modules/components-font-awesome/fonts/*'
+        'node_modules/font-awesome/fonts/*'
     ],
 
-    annotateEntry: [
+    jsEntry: [
 	'public/js/app.js',
 	'public/js/vendor.js'
     ]
 };
 
-gulp.task('build', ['build/js/app', 'build/js/vendor', 'build/css/vendor', 'build/fonts/vendor', 'build/ngAnnotate']);
+gulp.task('build', ['build/js/app', 'build/js/vendor', 'build/css/app', 'build/css/vendor', 'build/fonts/vendor', 'build/js/ngAnnotate', 'build/js/uglify']);
 gulp.task('default', ['clean', 'build']);
 
 //Clean public
@@ -70,8 +79,17 @@ gulp.task('clean', function() {
 gulp.task('build/js/app', function() {
     gulp.src(config.appJsEntry)
         .pipe(concat(config.outputJSAppFile))
+	.pipe(concatutil.header(';(function(window, undefined){\n'))
+	.pipe(concatutil.footer("\n}(window));"))
         .pipe(gulp.dest(config.outputJSDir));
 });
+
+gulp.task('build/css/app', function() {
+    gulp.src(config.appCssEntry)
+        .pipe(concat(config.outputAppCssFile))
+        .pipe(gulp.dest(config.outputCssDir));
+});
+
 
 // Build Vendors
 gulp.task('build/js/vendor', function() {
@@ -92,9 +110,15 @@ gulp.task('build/fonts/vendor', function () {
         .pipe(gulp.dest(config.outputFontsDir));
 });
 
-gulp.task('build/ngAnnotate', function () {
-    gulp.src(config.annotateEntry)
-        .pipe(ngAnnotate())
+gulp.task('build/js/ngAnnotate', function () {
+    gulp.src(config.jsEntry)
+        .pipe(ngAnnotate({ single_quotes: true }))
+        .pipe(gulp.dest(config.outputJSDir));
+});
+
+gulp.task('build/js/uglify', function () {
+    gulp.src(config.jsEntry)
+	.pipe(uglify())
         .pipe(gulp.dest(config.outputJSDir));
 });
 
